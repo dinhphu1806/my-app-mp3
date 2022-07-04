fee1da2f3dbca96d8a28ad9f376eeafed8ac93c7
dinhphu1806 <97866676+dinhphu1806@users.noreply.github.com>
2022-07-05 04:01:00 +0700

    mp3 initial

---
+++ js/music-list.js
@@ -0,0 +1,74 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [
+  {
+    name: "Âm thầm bên em - Version New 2022",
+    artist: "Sơn Tùng M-TP",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Ánh nắng của anh ",
+    artist: "Đức Phúc",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Chờ Đông",
+    artist: "Trung Quang",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Hơn cả yêu version",
+    artist: "Đức Phúc",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Nơi này có anh",
+    artist: "Sơn Tùng M-TP",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "Chắc ai đó sẽ về",
+    artist: "Sơn Tùng M-TP",
+    img: "music-6",
+    src: "music-6"
+  },
+  {
+    name: "Thất tình",
+    artist: "Trịnh Đình Quang",
+    img: "music-7",
+    src: "music-7"
+  },
+  {
+    name: "Ngày mai nắng lên anh sẽ về",
+    artist: "An Khang",
+    img: "music-8",
+    src: "music-8"
+  },
+  {
+    name: "Ba kể con nghe",
+    artist: "Dương Trần Nghĩa",
+    img: "music-9",
+    src: "music-9"
+  },
+  // like this paste it and remember to give comma after ending of this bracket }
+  // {
+  //   name: "Here is the music name",
+  //   artist: "Here is the artist name",
+  //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+  //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+  // }
+];
